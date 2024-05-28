abd7e2ffc4b62d8d7b35dfcfa2e5a15f52ad51d7
d1fner <159898204+d1fner@users.noreply.github.com>
2024-05-29 00:26:15 +0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,18 @@
+document.getElementById('pi').textContent = Math.PI;
+document.getElementById('e').textContent = Math.E;
+document.getElementById('log2e').textContent = Math.LOG2E;
+document.getElementById('ln2').textContent = Math.LN2;
+document.getElementById('ln10').textContent = Math.LN10;
+document.getElementById('log2').textContent = Math.LOG10E;
+document.getElementById('sqrt1_2').textContent = Math.SQRT1_2;
+document.getElementById('sqrt2').textContent = Math.SQRT2;
+
+
+document.getElementById('abs').textContent = Math.abs(-9);
+document.getElementById('sin').textContent = Math.sin(Math.PI / 2);
+document.getElementById('cos').textContent = Math.cos(0);
+document.getElementById('tan').textContent = Math.tan(Math.PI / 4);
+document.getElementById('log').textContent = Math.log(Math.E);
+
+
+document.getElementById('random').textContent = Math.random();
